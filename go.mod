4d93dbc0ca485b6292588142d8a2fbec29816473
Patryk Małek <malekpatryk+github@gmail.com>
2025-02-26 11:26:45 +0100

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pmalek/govulncheck1
+
+go 1.24.0
+
+require golang.org/x/crypto v0.35.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"golang.org/x/crypto/ed25519"
+)
+
+func main() {
+	const x = ed25519.PublicKeySize
+}
